e73239ef3c7804b170234937f3775ddceed0a28e
A1ex-N <azander724@gmail.com>
2022-12-04 18:28:10 +1000

    remove test data

--- anime_response.py
+++ anime_response.py
@@ -14,25 +14,3 @@ class AnimeResponse:
         if self.code != 2114:
             self.extra = json.loads(self.extra)["img_urls"]
             #self.image_urls = json.loads(self.extra)["img_urls"]
-
-success_data = {
-  "code": 0,
-  "msg": "",
-  "images": [],
-  "faces": [],
-  "extra": "{\"img_urls\": [\"https://activity.tu.qq.com/mqq/ai_painting_anime/share/2ff21130-739e-11ed-b6ea-525400dbb5e6.jpg\", \"https://activity.tu.qq.com/mqq/ai_painting_anime/share/2ff21130-739e-11ed-b6ea-525400dbb5e6.jpg\", \"https://activity.tu.qq.com/mqq/ai_painting_anime/share/2ff21130-739e-11ed-b6ea-525400dbb5e6.jpg\", \"https://activity.tu.qq.com/mqq/ai_painting_anime/share/2ff21130-739e-11ed-b6ea-525400dbb5e6.jpg\"], \"uuid\": \"2ff21130-739e-11ed-b6ea-525400dbb5e6\"}",
-  "videos": []
-}
-
-failure_data = {
-  "code": 2114,
-  "msg": "IMG_ILLEGAL"
-}
-
-if __name__ == "__main__":
-    res = AnimeResponse(**success_data)
-    print(res)
-    #print(res.image_urls)
-    #print(res)
-    #img_urls = json.loads(res.extra)["img_urls"]
-
