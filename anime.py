2180d0d5c38166a1d177cf0e2875f235af013a0e
A1ex-N <azander724@gmail.com>
2022-12-05 16:44:15 +1000

    alphabet to ALPHABET and remove comment

--- anime.py
+++ anime.py
@@ -3,14 +3,14 @@ from anime_post import AnimePost
 import random
 import sys
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Provide a filename")
         exit(1)
 
-    output_name = ''.join(random.choices(alphabet, k=6)) + ".jpg"
+    output_name = ''.join(random.choices(ALPHABET, k=6)) + ".jpg"
 
     if len(sys.argv) == 3:
         output_name = sys.argv[2]
@@ -18,5 +18,4 @@ if __name__ == "__main__":
     filename = sys.argv[1]
     print("Processing image: ", filename)
     anime = AnimePost.get_anime_image(filename)
-    save_anime_image(output_name, anime.extra[0]) 
-    #pprint(anime)
+    save_anime_image(output_name, anime.extra[0])
