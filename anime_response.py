1fb6833970f59a35cee03ba2d5acba085eda9589
Faris <76801659+0xDispatch@users.noreply.github.com>
2022-12-08 08:05:30 +0300

    Update anime_response.py
    new error code `ERROR_CODE_USER_IP_COUNTRY`

--- anime_response.py
+++ anime_response.py
@@ -5,6 +5,7 @@ import json
 ERROR_CODE_NUDITY = 2114
 ERROR_CODE_NO_FACE = 1001
 ERROR_CODE_AUTH_FAILED = -2111
+ERROR_CODE_USER_IP_COUNTRY = 2119
 
 
 @dataclass
@@ -25,6 +26,9 @@ class AnimeResponse:
             exit(1)
         if self.code == ERROR_CODE_NO_FACE:
             print("No face in image. Can't process")
+            exit(1)
+        if self.code == ERROR_CODE_USER_IP_COUNTRY:
+            print("Your ip is not from china, try using vpn or proxies.")
             exit(1)
 
         self.extra = json.loads(self.extra)["img_urls"]
