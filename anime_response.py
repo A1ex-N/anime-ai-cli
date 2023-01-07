1564f083d07787d46f9b7ad0d7634185f4a8f7df
Davr1 <42148912+Davr1@users.noreply.github.com>
2023-01-07 17:02:23 +0100

    Error codes

--- anime_response.py
+++ anime_response.py
@@ -1,11 +1,15 @@
 from dataclasses import dataclass, field 
 import json
+from enum import Enum
 
-
-ERROR_CODE_NUDITY = 2114
-ERROR_CODE_NO_FACE = 1001
-ERROR_CODE_AUTH_FAILED = -2111
-ERROR_CODE_USER_IP_COUNTRY = 2119
+class ErrorCodes(Enum):
+    SUCCESS = 0
+    NUDITY = 2114
+    NO_FACE = 1001
+    AUTH_FAILED = -2111
+    USER_IP_COUNTRY = 2119
+    PARAM_INVALID = -2100
+    CONNECTION_CLOSED = 141
 
 
 @dataclass
@@ -18,17 +22,24 @@ class AnimeResponse:
     videos: list = ""
 
     def __post_init__(self):
-        if self.code == ERROR_CODE_AUTH_FAILED:
-            print("Auth failed. Don't know how to solve this one")
-            exit(1)
-        if self.code == ERROR_CODE_NUDITY:
-            print("Image rejected. Nudity isn't allowed.")
-            exit(1)
-        if self.code == ERROR_CODE_NO_FACE:
-            print("No face in image. Can't process")
-            exit(1)
-        if self.code == ERROR_CODE_USER_IP_COUNTRY:
-            print("Your ip is not from china, try using vpn or proxies.")
-            exit(1)
+        success = False
 
-        self.extra = json.loads(self.extra)["img_urls"]
+        match ErrorCodes(self.code):
+            case ErrorCodes.AUTH_FAILED:
+                print("Auth failed. Don't know how to solve this one.")
+            case ErrorCodes.NUDITY:
+                print("Image rejected. Nudity isn't allowed.")
+            case ErrorCodes.NO_FACE:
+                print("No face in image. Can't process.")
+            case ErrorCodes.USER_IP_COUNTRY:
+                print("Your ip is not from china, try using vpn or proxies.")
+            case ErrorCodes.PARAM_INVALID:
+                print("Invalid file format. Must be one of jpeg|gif|png|bmp|ico|svg|tiff|ai|drw|pct|psp|xcf|psd|raw|webp.")
+            case ErrorCodes.CONNECTION_CLOSED:
+                print("The connection was forcibly closed by the host. (Is the image too big?)")
+            case ErrorCodes.SUCCESS:
+                self.extra = json.loads(self.extra)["img_urls"]
+                success = True
+
+        if not success:
+            exit(1)
